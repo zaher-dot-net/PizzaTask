ee78aa439ef378722821e16d3dca00441630dd23
Hussein Zaher <husseinzaher@outlook.com>
2025-09-30 01:12:59 +0300

    feat: pizza task

---
+++ Common/Food.cs
@@ -0,0 +1,8 @@
+namespace Common;
+
+public enum Food
+{
+    Pizza,
+    Burger,
+    Salad
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1;
+
+using Common;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your favorite food (Pizza, Burger, Salad): ");
+        string input = Console.ReadLine();
+        
+        // Try to parse user input into nullable enum
+        if (Enum.TryParse<Food>(input, true, out var chosenFood))
+        {
+            Console.WriteLine($"You chose: {chosenFood}");
+        }
+        else
+        {
+            Console.WriteLine("That food is not on the menu!");
+        }
+    }
+}
